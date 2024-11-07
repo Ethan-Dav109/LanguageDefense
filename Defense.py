344b726a8c772ca89aa25fa3d1a7d244d2ea60b5
Ethan-Dav109 <esd109@msstate.edu>
2024-11-07 11:34:01 -0600

    Updated Code
    This is the updated code that runs the PDF through tabula and converts it to a CSV then to and xlsx file immediately.

--- Defense.py
+++ Defense.py
@@ -1,4 +1,8 @@
-from tabula import convert_into
-table_file = r"2388Max.pdf"
-output_csv = r"2388max.csv"
-df = convert_into(table_file, output_csv, output_format='csv', lattice=True, stream=False, pages="all")
+import tabula
+import pandas as pd
+
+# convert PDF into CSV
+tabula.convert_into("2388Max.pdf", "2388Max.csv", output_format="csv", pages='all')
+
+read_file = pd.read_csv('2388Max.csv')
+read_file.to_excel('2388Max.xlsx', index=None, header=True)
